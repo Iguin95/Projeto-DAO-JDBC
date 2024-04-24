66fd3d06e51a0ae0f5b93df13a7e3fae92d535ba
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-24 17:10:43 -0300

    Criação da classe Departament e Seller

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.time.LocalDate;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		LocalDate data = LocalDate.now();
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", data, 3000.0, obj);
+		System.out.println(seller);
+		
+		//git não está aceitando 
+	}
+	
+	
+}
