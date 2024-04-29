964ac958470c861a7ae5c4c432038c7699a0820e
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-29 14:19:39 -0300

    Implementação do método delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
@@ -12,6 +13,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		// injeção de dependência sem explicitar a implementação, dessa forma eu chamo a
 		// fábrica de DAO que através do método, instanciará e criara o SellerDAO
 		SellerDAO sellerDao = DaoFactory.createSellerDao();
@@ -43,6 +45,14 @@ public class Program {
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update Completed!");
+		
+		System.out.println("\n< ============ TEST 6: seller Delete ============ > ");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
