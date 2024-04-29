c189e6ac2d31c56aa235a8f60247afaf97be2bb3
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-29 13:37:49 -0300

    Implementação do método insert

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -6,7 +6,7 @@ import model.dao.impl.SellerDaoJDBC;
 //Fábrica: Classe auxiliar responsável por instanciar os meus DAO's através de métodos estáticos
 public class DaoFactory {
 	
-	//método que instanciará e retornará um SellerDAO
+	//Método que instanciará e retornará um SellerDAO
 	//Dessa forma, minha classe vai expor um método que retorna um tipo da Interface, mas internamente ela vai instanciar uma implementação que ficará oculta para o programa principal
 	public static SellerDAO createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,17 +18,57 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDAO{
 	
-	//a nossa classe, terá uma dependência com a conexão com o db
-	private Connection conn; //esse objeto de conexão, estará disponível por toda minha classe
+	//A nossa classe, terá uma dependência com a conexão com o db
+	private Connection conn; //Esse objeto de conexão, estará disponível por toda minha classe
 	
-	//injeção de dependência para conexão com o db
+	//Injeção de dependência para conexão com o db
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) " 
+					+ "VALUES " 
+					+"(?, ?, ?, ?, ?)",//PlaceHolders.
+					Statement.RETURN_GENERATED_KEYS //Retorna o id do novo objeto inserido.
+					);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId()); //Associação de classes.
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				//O próximo bloco é um 'if' pois será inserido somente um objeto novo.
+				if(rs.next()) {
+					int id = rs.getInt(1); //Pegando o 'id' gerado.
+					obj.setId(id); /*Inserindo no novo objeto Seller passado como argumento
+					neste método o id capturado.*/
+				}
+				DB.ResultSet(rs); /*Foi criado dentro do escopo do 'if', então tenho que 
+				fecha-lo dentro do mesmo escopo, pois fora, ele não existe.*/
+				
+			} //Caso na inserção, nenhuma linha for afetada, será lançada uma exceção.
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -158,14 +199,29 @@ public class SellerDaoJDBC implements SellerDAO{
 			rs = st.executeQuery();
 			
 			List<Seller> list = new ArrayList<Seller>();
+			
+			//O map será responsável por ligar o nome do departamento com o seu número de identificação.
 			Map<Integer, Department> map = new HashMap<>();
 			
-			while(rs.next()) { //enquanto tiver um próximo irá percorrer
+			while(rs.next()) { //Enquanto tiver um próximo irá percorrer
+				
+				/*Variável 'dep' irá receber o nome do departamento, pois a chave é o id
+				 * e o valor é o nome do departamento.*/
 				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				/*Caso não haja departamento existente, entrará neste 'if' abaixo
+				 * e instanciará um novo departamento, adicionando na estrutura
+				 * de dados map, o id do departamento mais o nome do departamento que
+				 * foi obtido acima.*/
 				if(dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
+				
+				/*Caso o 'if' falha, quer dizer que o departamento já existe e ele será
+				 * reaproveitado, o que faz com que ele não instanciado a cada objeto
+				 * vendedor criado. Um único departamento, com o mesmo id será utilizado
+				 * para todos os vendedores.*/
 				Seller obj = instantiateSeller(rs, dep);
 				
 				list.add(obj);
