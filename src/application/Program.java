39458ea6df81bd8cce264bd0f48f6af3f09238ea
Igor Vinnícius <igorquintao22@gmail.com>
2024-04-29 13:56:27 -0300

    Implementação do método update

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,11 +33,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n< ============ TEST 4: seller insert ============ > ");
+		System.out.println("\n< ============ TEST 4: seller Insert ============ > ");
 		Seller newSeller = new Seller(null, "Kalita", "kali@gmail.com", new Date(), 9000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n< ============ TEST 5: seller Update ============ > ");
+		seller = sellerDao.findById(1); //peguei o vendedor de id igual a 1.
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
 	}
 
 }
